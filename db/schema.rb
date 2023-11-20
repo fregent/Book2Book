89c818abe5de1f443cc79797f06db02983362435
Frederic REGENT <frederic.regent97@gmail.com>
2023-11-20 16:04:42 +0100

    Deleted models. User model generated with devise gem. Book model generated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_142256) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_150351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,41 +26,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_142256) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
-  create_table "reservations", force: :cascade do |t|
-    t.datetime "time"
-    t.bigint "user_id", null: false
-    t.bigint "book_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_reservations_on_book_id"
-    t.index ["user_id"], name: "index_reservations_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.text "content"
-    t.float "rating"
-    t.bigint "user_id", null: false
-    t.bigint "book_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_reviews_on_book_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password"
-    t.string "address"
-    t.date "birthdate"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "books", "users"
-  add_foreign_key "reservations", "books"
-  add_foreign_key "reservations", "users"
-  add_foreign_key "reviews", "books"
-  add_foreign_key "reviews", "users"
 end
