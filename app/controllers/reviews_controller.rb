af9d979e5df3b6a64ed80209aa82139378571567
Frederic REGENT <frederic.regent97@gmail.com>
2023-11-21 16:01:12 +0100

    model view controller for books

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,13 @@
 class ReviewsController < ApplicationController
- 
+
   def create
+    @review = Review.new(params_review)
+    @book = Book.find(params[:book_id])
+    @review.book = @book
+    if @review.save
+      redirect_to book_path(@book)
+    else
+      render "books/show"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'reviews/new'
   get 'reviews/create'
   devise_for :users
-  root to: "pages#home"
+  root to: "books#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
