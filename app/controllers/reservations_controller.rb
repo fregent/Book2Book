ae1317afa6ef09332bf2bb7315f63de9df5e5b70
Frederic REGENT <frederic.regent97@gmail.com>
2023-11-21 16:11:16 +0100

    controllers merged

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,7 +2,6 @@ class ReservationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-<<<<<<< HEAD
     @reservations = Reservation.all
   end
 
@@ -10,18 +9,15 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
     @book = @reservation.book
     @user = current_user
-=======
     @reservations = current_user.reservations
   end
 
   def show
     @reservation = current_user.reservations.find(params[:id])
->>>>>>> master
   end
 
   def new
     @reservation = Reservation.new
-<<<<<<< HEAD
     @book = Book.find(params[:book_id])
     @user = current_user
   end
@@ -35,7 +31,6 @@ class ReservationsController < ApplicationController
       redirect_to book_path(@book)
     else
       render "books/show"
-=======
   end
 
   def create
@@ -45,7 +40,6 @@ class ReservationsController < ApplicationController
       redirect_to reservation_path(@reservation), notice: 'Your book has been booked!'
     else
       render :new, status: :unprocessable_entity
->>>>>>> master
     end
   end
 
