d69d37ee408d3cb545ece20c215961aa4a41d673
Frederic REGENT <frederic.regent97@gmail.com>
2023-11-21 16:45:20 +0100

    validations removed for test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :reservations
          has_many :reviews
-         validates :first_name, presence: true
-         validates :last_name, presence: true
+        #  validates :first_name, presence: true
+        #  validates :last_name, presence: true
          validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
          validates :password, presence: true, length: { minimum: 6 }
-         validates :address, presence: true
-         validates :birthdate, presence: true
-         validates :payment_info, presence: true
-
+        #  validates :address, presence: true
+        #  validates :birthdate, presence: true
+        #  validates :payment_info, presence: true
 end
