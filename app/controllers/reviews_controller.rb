53bcc3719787728ea156b81bddab288aff4d120c
Andre Leroy <a.l.sismondino@outlook.fr>
2023-11-21 15:14:06 +0100

    controller's actions added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
- 
+  before_action :authenticate_user!
+  
   def create
+    @book = Book.find(params[:book_id])
+    @review = Review.new(review_params)
+    @review.book = @book
+    if @review.save
+      redirect_to book_path(@book)
+    else
+      render "books/show", status: :unprocessable_entity
+    end
+
   end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:rating, :content)
+    end
 end
