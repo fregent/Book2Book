60d347a082115a22292776690beaff5872413b5f
Andre Leroy <a.l.sismondino@outlook.fr>
2023-11-22 12:06:17 +0100

    book creation working good

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,16 @@ class ReviewsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @review = Review.new(review_params)
+    @review.user = current_user
     @review.book = @book
     if @review.save
       redirect_to book_path(@book)
     else
+      @reviews = @book.reviews
+      @user = current_user
+      @user_book = @book.user
+      @reservation = Reservation.new
+      @reviews = @book.reviews
       render "books/show", status: :unprocessable_entity
     end
   end
